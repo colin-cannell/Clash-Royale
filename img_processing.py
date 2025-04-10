e776b83998b7d94930cb4da8650f321802cc0d74
Colin Cannell <92816299+colin-cannell@users.noreply.github.com>
2025-04-10 16:52:16 -0400

    Update img_processing.py

--- img_processing.py
+++ img_processing.py
@@ -1,28 +1,39 @@
 import cv2 as cv
 import pyautogui as pg
+import numpy as np
 
-player_dim = ()
-opponent_dim = ()
+player_dim = (0, 600, 450, 625)
+opponent_dim = (0, 600, 225, 325)
+
 
 def take_screenshot(dim):
     # Use pyautogui or similar library to take a screenshot
+
     screenshot = pg.screenshot()
+    # screenshot.save("screenshot.png")
+    screenshot = cv.imread("screenshot.png")
+
     # Load the screenshot using OpenCV
-    img = cv.cvtColor(cv.imread(screenshot), cv.COLOR_RGB2BGR)
-    x1, y1, x2, y2 = dim
-    # Crop the image to the specified dimensions
-    cropped_img = img[y1:y2, x1:x2]
-    cv.imshow("Cropped Image", cropped_img)
+    screenshot = np.array(screenshot)
+    img = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
+    if len(dim) == 4:
+        x1, x2, y1, y2 = dim
+        img = img[y1:y2, x1:x2]
+    else:
+        x1, x2 = dim
+        img = img[:, x1:x2]
+    cv.imshow("Cropped Image", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-
-def process_sequence(image):
-    pass
-
-def get_sequence_image():
-    pass
 
 def main():
-    pass
+    while True:
+        if pg.keyDown("SPACE"):
+            take_screenshot(player_dim)
+            take_screenshot(opponent_dim)
+
+take_screenshot(opponent_dim)
 
 # when space is pressed, means new card picks
 
